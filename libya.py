6136b83267666ecefe835a55eb1869eb0c8f584b
iamomarsalem <117285879+iamomarsalem@users.noreply.github.com>
2022-11-26 20:59:11 +0200

    Update libya.py

--- libya.py
+++ libya.py
@@ -1,9 +1,14 @@
 #libya
-print("[1] how old are you")  
-print("[2] whats your name") 
+print("[1]ترضعه ولا اديره مرضعه ولا تنقز من الروشن")
+print("[2] مص زبي")
+print("[3] فقص")
+print("فيسع") 
 omar = input("akhtar")
-if omar in["1"]:
-  print("14")
-  if omar in["2"]:
-    print("my name is omar")
+if omar in["1"}: 
+           print("علاش ترضع فيه تحساب روحك ونيس؟ ")
+if omar in["2"]: 
+           print(" معش تمص زبي مخليه لونيس بس يمصه") 
+      if omar in["3"]: 
+           print("لنقنك تفقص احسن من ونيس")
+    
     
